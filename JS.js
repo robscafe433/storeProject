8e4c818342ac554626cbf9be8512631b8d267a99
luis velazquez <javiervelazquez113@yahoo.com>
2021-01-21 21:56:56 -0800

    complete back to top CTA

--- JS.js
+++ JS.js
@@ -393,17 +393,3 @@ function displayCart() {
 
 onLoadCartNumbers();
 displayCart();
-
-
-// back to top function
-//Get the button:
-const mybutton = document.getElementById("scrollup");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
