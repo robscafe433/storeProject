9d603e0d4b198843d4d7d79e39fe452acac2730d
VelazquezLuis <javiervelazquez113@yahoo.com>
2021-03-20 11:10:51 -0700

    cart js edits to render

--- loadPageData.js
+++ loadPageData.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let path = window.location.pathname;
     let pageFile = path.split("/").pop();
+
     let pageName = pageFile.split(".")[0];
 
     const productspa = new Products();
@@ -61,36 +62,54 @@ document.addEventListener("DOMContentLoaded", () => {
 
         Object.values(results).map((items) => {
             console.log("Inside MAP ***", items, items.name);
-            secondRowPA.innerHTML += ` 
-            <div class="col-3 second-row-PA  ">
-                <img src="${items.image}" height="40">
-            </div>
-            <div class="col-3 second-row-PA ">
-                <h4>${items.name}</h4>
-            </div>
-            <div class="col-3 align-self-center second-row-PA ">
-                <div class="input-group w-75 px-1 mb-1">
-                    <div class="input-group-prepend">
-                        <button class="btn btn-outline-secondary px-2" type="button">
-                            -
-                        </button>
-                    </div>
-
-                    <input type="text" class="form-control" placeholder="Qty" value="${items.inCart}" aria-label="Quantity" aria-describedby="basic-addon1">
-
-                    <div class="input-group-prepend">
-                        <button class="btn btn-outline-secondary px-2" type="button">
-                            +
-                        </button>
+            secondRowPA.innerHTML += `
+            <div class="col-12 py-2">
+            <div class="row">
+                <div class="col-3 align-self-center second-row-PA">
+                    <img src="${items.image}" height="70px" />
+                </div>
+                <div class="col-3 align-self-center second-row-PA">
+                    <h4>${items.name}</h4>
+                </div>
+                <div class="col-3 align-self-center second-row-PA">
+                    <div class="input-group w-75 px-1 mb-1">
+                        <div class="input-group-prepend">
+                            <button
+                                class="btn btn-outline-secondary px-2"
+                                type="button"
+                            >
+                                -
+                            </button>
+                        </div>
+        
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Qty"
+                            value="${items.inCart}"
+                            aria-label="Quantity"
+                            aria-describedby="basic-addon1"
+                        />
+        
+                        <div class="input-group-prepend">
+                            <button
+                                class="btn btn-outline-secondary px-2"
+                                type="button"
+                            >
+                                +
+                            </button>
+                        </div>
                     </div>
                 </div>
+               <div class="col-2 align-self-center">
+                    <h5>$${items.price}</h5>
+                </div>
+                <div class="col-1 align-self-center second-row-PA">
+                    <button class="badge text-white bg-secondary rounded-pill">X</button>
+                </div>
             </div>
-                                
-            <div class="col-3 second-row-PA">
-                <h4>X</h4>
-            </div>
-                               
-            `;
+        </div>
+        `;
         });
     }
 });
