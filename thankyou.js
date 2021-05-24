a0055e708526bc654e7ca1d2a80943541ec581bf
Robert S <robscafe333@yahoo.com>
2021-05-24 11:01:56 -0700

    continued adding middle section of delivery info on thankyou.html

--- thankyou.js
+++ thankyou.js
@@ -1,12 +1,12 @@
 let date = new Date();
 
 let month = date.getMonth();
-let dates = date.getDate();
+let calendarNumericalDay = date.getDate();
 let weekday = date.getDay();
 let processingDays = "null";
 let shipOutOnWeekDay = "null";
 
-let weekdays = [
+let weekdaysArray = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -19,55 +19,47 @@ let weekdays = [
 let eta = document.querySelector(".eta");
 
 // Delivery Dates: -------------------------------------------------------
+console.log(weekday);
+
+//0-Sunday ... Saturday-6
 
 if (weekday == 0) {
     // Sunday orders:
     shipOutOnWeekDay = 3; // Wednesday
     processingDays = 3;
-    console.log("Will be shipped out on Wednesday");
-    console.log(month, "/", dates + processingDays);
     eta.innerHTML = `
-        <h5>${weekdays[shipOutOnWeekDay]}, 9AM - 11AM</h5>
-        <h5>${month + 1}/${dates + processingDays}</h5>
+        <h5>${weekdaysArray[shipOutOnWeekDay]}, 9AM - 11AM</h5>
+        <h5>${month + 1}/${calendarNumericalDay + processingDays}</h5>
     `;
 } else if (weekday == 6) {
     // Saturday orders:
     shipOutOnWeekDay = 3; // Wednesday
     processingDays = 4; //processing Monday for shipping on Wednesday
-    shippingDate = dates + processingDays;
-    console.log("Will be shipped out on Wednesday");
-    console.log(month + 1, "/", dates + processingDays);
     eta.innerHTML = `
-        <h5>${weekdays[shipOutOnWeekDay]}, 9AM - 11AM</h5>
-        <h5>${month + 1}/${dates + processingDays}</h5>
+        <h5>${weekdaysArray[shipOutOnWeekDay]}, 9AM - 11AM</h5>
+        <h5>${month + 1}/${calendarNumericalDay + processingDays}</h5>
     `;
 } else if (weekday == 4) {
     // Thursday orders:
+    shipOutOnWeekDay = 1; // Monday
     processingDays = 4; //processing Thursday for shipping on Monday
-    shippingDate = dates + processingDays;
-    console.log("Will be shipped out on Tuesday");
-    console.log(month, "/", dates + processingDays);
     eta.innerHTML = `
-        <h5>${weekdays[shipOutOnWeekDay]}, 9AM - 11AM</h5>
-        <h5>${month + 1}/${dates + processingDays}</h5>
+        <h5>${weekdaysArray[shipOutOnWeekDay]}, 9AM - 11AM</h5>
+        <h5>${month + 1}/${calendarNumericalDay + processingDays}</h5>
     `;
 } else if (weekday == 5) {
     // Friday orders:
     processingDays = 4; //processing Friday for shipping on Tuesday
-    shippingDate = dates + processingDays;
-    console.log("Will be shipped out on Tuesday");
-    console.log(month, "/", dates + processingDays);
+    shipOutOnWeekDay = 2; // Tuesday
     eta.innerHTML = `
-        <h5>${weekdays[shipOutOnWeekDay]}, 9AM - 11AM</h5>
-        <h5>${month + 1}/${dates + processingDays}</h5>
+        <h5>${weekdaysArray[shipOutOnWeekDay]}, 9AM - 11AM</h5>
+        <h5>${month + 1}/${calendarNumericalDay + processingDays}</h5>
     `;
 } else {
-    shippingDate = weekday + processingDays;
+    processingDays = 2;
+    shipOutOnWeekDay = weekday + processingDays;
     eta.innerHTML = `
-        <h5>${weekdays[shipOutOnWeekDay]}, 9AM - 11AM</h5>
-        <h5>${month + 1}/${dates + processingDays}</h5>
+        <h5>${weekdaysArray[shipOutOnWeekDay]}, 9AM - 11AM</h5>
+        <h5>${month + 1}/${calendarNumericalDay + processingDays}</h5>
     `;
 }
-
-// Delivery info: ------------------------------------------------------
-
