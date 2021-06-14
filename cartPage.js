c8fcee2e3b5c5eb683c7070f75f0da2daf53ac9e
luis velazquez <javiervelazquez113@yahoo.com>
2021-06-13 19:58:45 -0700

    added funtction to disable checkout button

--- cartPage.js
+++ cartPage.js
@@ -209,20 +209,24 @@ class MainCartPageClass {
     }
 }
 
-// Saving shipping value in storage  
+
+// Saving shipping value in storage
 document.addEventListener("DOMContentLoaded", () => {
-let formSelection = document.getElementById(
-  "exampleFormControlSelect1"
-);
+    let formSelection = document.getElementById("exampleFormControlSelect1");
+    let checkoutBtn = document.getElementById("checkoutButton");
+    if (sessionStorage["exampleFormControlSelect1"]) {
+        // if exampleFormControlSelect1 is set
+        formSelection.value = sessionStorage["exampleFormControlSelect1"]; // set the value
+    }
 
-if (sessionStorage["exampleFormControlSelect1"]) {
-  // if exampleFormControlSelect1 is set
-  formSelection.value = sessionStorage["exampleFormControlSelect1"]; // set the value
-  
-}
-
-formSelection.onchange = function () {
-  
-  sessionStorage["exampleFormControlSelect1"] = this.value; // change sessionStorage on change
-}
+    formSelection.onchange = function () {
+        sessionStorage["exampleFormControlSelect1"] = this.value; // change sessionStorage on change
+    };
+    // enabling and disabling checkout button
+    if (formSelection.value === "null") {
+        checkoutBtn.disabled = true;
+    } else {
+        checkoutBtn.disabled = false;
+    }
 });
+
